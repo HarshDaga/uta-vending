c67da610b5601fe10d995b29ed1146a273018a3b
Harsh Daga <harsh.daga.tech@gmail.com>
2020-05-07 00:23:12 -0500

    Fix UI constraints

--- app/src/main/java/com/uta/vending/UserHomeScreen.java
+++ app/src/main/java/com/uta/vending/UserHomeScreen.java
@@ -1,12 +1,11 @@
 package com.uta.vending;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.content.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.*;
 
 public class UserHomeScreen extends AppCompatActivity {
     Button btnLogout;
@@ -26,7 +25,7 @@ public class UserHomeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_after_login);
+	    setContentView(R.layout.activity_user_homescreen);
 
         id = getIdFromIntent();
 
